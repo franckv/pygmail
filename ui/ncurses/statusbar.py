0ccd10ce152ab5ad93f60fb0ce605ab38f066908
franck <franck.vdl@gmail.com>
2010-05-13 13:34:04 +0200

    Move widgets to subpackage

--- ui/ncurses/statusbar.py
+++
@@ -1,6 +0,0 @@
-from textbox import TextBox
-
-class StatusBar(TextBox):
-    def __init__(self, parent):
-        super(StatusBar, self).__init__(parent, -2)
-
--- ui/ncurses/titlebar.py
+++
@@ -1,6 +0,0 @@
-from textbox import TextBox
-
-class TitleBar(TextBox):
-    def __init__(self, parent):
-        super(TitleBar, self).__init__(parent, 0)
-
