8fcdb039ca62b12f2525fe36f4bf74262c71e9d0
franck <franck.vdl@gmail.com>
2010-05-13 13:36:15 +0200

    ui fixes and improvments

--- ui/ncurses/__init__.py
+++ ui/ncurses/__init__.py
@@ -4,52 +4,23 @@ import curses
 import locale
 
 import common
-from screen import Screen
+from widget import Screen
+from commandhandler import CommandHandler
 
 locale.setlocale(locale.LC_ALL, '')
 
 def main(stdscr):
     screen = Screen(stdscr)
 
-    screen.encoding = locale.getpreferredencoding()
+    screen.set_encoding(locale.getpreferredencoding())
 
     screen.title.set_text('%s v%s' % (common.PROGNAME, common.PROGVERSION))
     screen.status.set_text('Standby ready')
 
     screen.refresh()
 
-    while True:
-        c = screen.command.get_char()
-
-        if c is None:
-            continue
-
-        (y, x) = screen.get_pos()
-        screen.status.set_text('(%i, %i) : <%s>' % (y, x, c.strip()))
-
-        if c == '<KEY_LEFT>':
-            (y, x) = screen.get_pos()
-            screen.move(y, x-1)
-        elif c == '<KEY_RIGHT>':
-            (y, x) = screen.get_pos()
-            screen.move(y, x+1)
-        elif c == '<KEY_DOWN>':
-            screen.win.scroll(1)
-            screen.refresh()
-        elif c == '<KEY_UP>':
-            screen.win.scroll(-1)
-            screen.refresh()
-        elif c == '<KEY_RESIZE>':
-            screen.resize()
-        elif c == ':':
-            screen.read_command()
-        elif c == '<KEY_ENTER>' or c == '\n':
-            (y, x) = screen.get_pos()
-            (maxy, maxx) = screen.win.getmaxyx()
-            if y < maxy - 3:
-                screen.move(y+1, 0)
-        else:
-            screen.main.write(c)
+    handler = CommandHandler(screen)
+    handler.handle()
 
 def run(args = None):
     curses.wrapper(main)
---
+++ ui/ncurses/commandhandler.py
@@ -0,0 +1,46 @@
+class CommandHandler:
+    def __init__(self, screen):
+        self.screen = screen
+        screen.set_handler(self)
+        self.buf = ''
+
+    def handle(self):
+        while True:
+            c = self.screen.get_char()
+
+            if c is None:
+                continue
+
+            (y, x) = self.screen.get_pos()
+            self.screen.set_status('(%i, %i) : <%s>' % (y, x, c.strip()))
+            if c == '<KEY_LEFT>':
+                (y, x) = self.screen.get_pos()
+                self.screen.move(y, x-1)
+            elif c == '<KEY_RIGHT>':
+                (y, x) = self.screen.get_pos()
+                self.screen.move(y, x+1)
+            elif c == '<KEY_DOWN>':
+                self.screen.win.scroll(1)
+                self.screen.refresh()
+            elif c == '<KEY_UP>':
+                self.screen.win.scroll(-1)
+                self.screen.refresh()
+            elif c == '<KEY_RESIZE>':
+                self.screen.resize()
+            elif c == ':':
+                cmd = self.screen.read_command()
+                self.screen.set_status('(%i, %i) : <%s>' % (y, x, cmd.strip()))
+                self.run_command(cmd)
+            elif c == '<KEY_ENTER>' or c == '\n':
+                (y, x) = self.screen.get_pos()
+                (maxy, maxx) = self.screen.win.getmaxyx()
+                if y < maxy - 3:
+                    self.screen.move(y+1, 0)
+            else:
+                pass
+                #screen.main.write(c)
+
+    def run_command(self, cmd):
+        if cmd == 'q' or cmd == 'quit':
+            self.screen.destroy()
+
